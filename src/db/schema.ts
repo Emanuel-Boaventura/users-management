c47594504a57fcc32f35241b62009733bdf81ce8
Emanuel-Boaventura <emanuelbm.dev@gmail.com>
2025-07-01 12:32:34 -0400

    feat: create base structure for the project

---
+++ src/db/schema.ts
@@ -0,0 +1,18 @@
+import { integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
+
+const createdAt = timestamp("created_at", { withTimezone: true })
+  .notNull()
+  .defaultNow();
+const updatedAt = timestamp("updated_at", { withTimezone: true })
+  .notNull()
+  .defaultNow()
+  .$onUpdate(() => new Date());
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  age: integer().notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  createdAt,
+  updatedAt,
+});
