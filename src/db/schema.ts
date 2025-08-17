3361ee0fb5faaf91044be34d8ea3077d250c7184
Emanuel-Boaventura <emanuelbm.dev@gmail.com>
2025-08-16 23:56:04 -0400

    fix: old lint errors

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,18 +1,16 @@
 import { integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
 
-const createdAt = timestamp("created_at", { withTimezone: true })
-  .notNull()
-  .defaultNow();
+const createdAt = timestamp("created_at", { withTimezone: true }).notNull().defaultNow();
 const updatedAt = timestamp("updated_at", { withTimezone: true })
   .notNull()
   .defaultNow()
   .$onUpdate(() => new Date());
 
 export const usersTable = pgTable("users", {
+  age: integer().notNull(),
+  createdAt,
+  email: varchar({ length: 255 }).notNull().unique(),
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-  createdAt,
   updatedAt,
 });
