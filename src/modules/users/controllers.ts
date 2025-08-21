512758be3a89756fbf09fee8113b78775da9e40e
Emanuel-Boaventura <emanuelbm.dev@gmail.com>
2025-08-21 18:44:32 -0400

    feat: minor changes

--- src/modules/users/controllers.ts
+++ src/modules/users/controllers.ts
@@ -6,7 +6,7 @@ async function getUsers(req: Request, res: Response) {
   try {
     const users = await UserServices.findAll();
 
-    res.send(users);
+    res.send({ data: users });
   } catch (error) {
     res.status(500).send({ error });
   }
@@ -34,9 +34,9 @@ async function createUser(req: Request, res: Response) {
       return res.status(400).send(JSON.parse(result.error.message));
     }
 
-    const createdUser = await UserServices.createUser(result.data);
+    const [createdUser] = await UserServices.createUser(result.data);
 
-    res.send(createdUser[0]);
+    res.send(createdUser);
   } catch (error) {
     res.status(500).send({ error });
   }
@@ -45,13 +45,14 @@ async function createUser(req: Request, res: Response) {
 async function updateUser(req: Request, res: Response) {
   try {
     const result = updateUserSchema.safeParse(req.body);
+
     if (!result.success) {
       return res.status(400).send(result.error);
     }
 
-    const updatedUser = await UserServices.updateUser(req.params.id, result.data);
+    const [updatedUser] = await UserServices.updateUser(req.params.id, result.data);
 
-    res.send(updatedUser[0]);
+    res.send(updatedUser);
   } catch (error) {
     res.status(500).send({ error });
   }
@@ -61,7 +62,11 @@ async function deleteUserById(req: Request, res: Response) {
   try {
     const deletedUser = await UserServices.deleteById(req.params.id);
 
-    res.send(deletedUser[0]);
+    if (deletedUser.length === 0) {
+      return res.status(404).send("User not found");
+    }
+
+    res.send(deletedUser);
   } catch (error) {
     res.status(500).send({ error });
   }
